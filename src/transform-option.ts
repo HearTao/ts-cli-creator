61f2553b9b6c80d03657abb6670c1d5d573d2fdc
Rabbit <yfhj1990@hotmail.com>
2019-07-28 09:36:46 +0800

    Add convert enum to choices

--- src/transform-option.ts
+++ src/transform-option.ts
@@ -1,12 +1,118 @@
-import { ts, JSDoc, JSDocTag, PropertySignature, InterfaceDeclaration, JSDocableNode } from 'ts-morph'
+import { ts, JSDoc, JSDocTag, PropertySignature, InterfaceDeclaration, JSDocableNode, Type, Node, EnumDeclaration } from 'ts-morph'
+import { Options as YargsOptions } from 'yargs'
+// import {} from 'ts-creator'
 
-// enum YargsType {
-//   String = 'string',
-//   Number = 'number',
-//   Boolean = 'boolean'
-//   /**@todo Count = 'count'*/ 
-// }
+const enum YargsType {
+  String,
+  Number,
+  Boolean,
+  ArrayString,
+  ArrayNumber,
+  ArrayBoolean,
+  /**@todo */Enum
+}
 
+export const enum YargsControlledTag {
+  Type = 'type',
+  Array = 'array',
+  Choices = 'choices',
+  Description = 'description',
+  Desc = 'desc',
+  Describe = 'describe'
+}
+
+const YargsControlledTags: string[] = [
+  YargsControlledTag.Type,
+  YargsControlledTag.Array,
+  YargsControlledTag.Choices,
+  YargsControlledTag.Description,
+  YargsControlledTag.Describe,
+  YargsControlledTag.Desc
+]
+
+export const enum YargsSupportedTag {
+  Alias = 'alias',
+  Default = 'default'
+}
+
+const YargsSupportedTags: string[] = [
+  YargsSupportedTag.Alias,
+  YargsSupportedTag.Default
+]
+
+type MappedYargsType = 
+  | [ YargsType.String, null ]
+  | [ YargsType.Number, null ]
+  | [ YargsType.Boolean, null ]
+  | [ YargsType.ArrayString, null ]
+  | [ YargsType.ArrayNumber, null ]
+  | [ YargsType.ArrayBoolean, null ]
+  | [ YargsType.Enum, EnumDeclaration ]
+
+function mapToYargsType(type: Type): MappedYargsType {
+  const text = type.getText()
+  switch(text) {
+    case `string`: return [ YargsType.String, null ]
+    case `number`: return [ YargsType.Number, null ]
+    case `boolean`: return [ YargsType.Boolean, null ]
+    case `string[]`: 
+    case `Array<string>`: return [ YargsType.ArrayString, null ]
+    case `number[]`: 
+    case `Array<number>`: return [ YargsType.ArrayNumber, null ]
+    case `boolean[]`:
+    case `Array<boolean>`: return [ YargsType.ArrayBoolean, null ]
+    default: {
+      const symbol = type.getSymbol()
+      if(undefined === symbol) throw makeUnsupportsTypeError(text)
+      const decls = symbol.getDeclarations()
+      if(0 === decls.length) throw makeUnsupportsTypeError(text)
+      const decl = decls[0]
+      if(!isEnumDeclaration(decl)) throw makeUnsupportsTypeError(text)
+      return [ YargsType.Enum, decl ]
+    }
+  }
+}
+
+function isEnumDeclaration(node: Node): node is EnumDeclaration {
+  return ts.SyntaxKind.EnumDeclaration === node.getKind()
+}
+
+function transformYargsType(mappedType: MappedYargsType): { [key: string]: ts.Node } {
+  switch(mappedType[0]) {
+    case YargsType.String: return { [YargsControlledTag.Type]: ts.createLiteral(`string`) }
+    case YargsType.Number: return { [YargsControlledTag.Type]: ts.createLiteral(`number`) }
+    case YargsType.Boolean: return { [YargsControlledTag.Type]: ts.createLiteral(`boolean`) }
+    case YargsType.ArrayString: return { [YargsControlledTag.Type]: ts.createLiteral(`string`), [YargsControlledTag.Array]: ts.createLiteral(true) }
+    case YargsType.ArrayNumber: return { [YargsControlledTag.Type]: ts.createLiteral(`number`), [YargsControlledTag.Array]: ts.createLiteral(true) }
+    case YargsType.ArrayBoolean: return { [YargsControlledTag.Type]: ts.createLiteral(`boolean`), [YargsControlledTag.Array]: ts.createLiteral(true) }
+    case YargsType.Enum: return makeEnumOption(mappedType[1])
+    default: throw makeUnknownYargsTypeError(mappedType[0])
+  }
+}
+
+function makeEnumOption(enumDecl: EnumDeclaration): ReturnType<typeof transformYargsType> {
+  const enumName: string = enumDecl.getName()
+  const members = enumDecl.getMembers()
+  const nodes = members.map(member => {
+    const name: string = member.getName()
+    return ts.createPropertyAccess(
+      ts.createIdentifier(enumName),
+      ts.createIdentifier(name)
+    )
+  })
+  return {
+    [YargsControlledTag.Type]: ts.createLiteral(`string`),
+    [YargsControlledTag.Choices]: ts.createArrayLiteral(nodes, false)
+  }
+}
+
+function makeUnsupportsTypeError(type: string): Error {
+  return new Error(`Unsupports type "${type}"`)
+}
+
+function makeUnknownYargsTypeError(yargsType: YargsType): Error {
+  return new Error(`Unknown yargsType "${yargsType}"`)
+}
 
 // function getTag(jsdoc: JSDoc, predicate: (tag: JSDocTag) => boolean): JSDocTag | undefined {
 //   return jsdoc.getTags().find(tag => predicate(tag))
@@ -37,15 +143,15 @@ function renderCallable(args: ts.Expression[]): ts.CallExpression {
   )
 }
 
-function render(name: string, props: { [key: string]: string }): [ts.Node, ts.Node] {
+function render(name: string, props: { [key: string]: ts.Node }): [ts.Node, ts.Node] {
   const objects = []
   for (const key in props) {
     if (props.hasOwnProperty(key)) {
-      const value = props[key]
+      const value = props[key as keyof YargsOptions]
       objects.push(
         ts.createPropertyAssignment(
           ts.createIdentifier(key),
-          ts.createStringLiteral(value)
+          value as ts.Expression
         )
       )
     }
@@ -60,15 +166,19 @@ function render(name: string, props: { [key: string]: string }): [ts.Node, ts.No
   ]
 }
 
-function transformInterfaceProps(props: PropertySignature[]): [ string, { [key: string]: string } ][] {
+
+function transformInterfaceProps(props: PropertySignature[]): [ string, { [key: string]: ts.Node } ][] {
   return props.map(prop => {
     const name: string = prop.getName()
-    const type: string = prop.getType().getText()
+    
+    const type = prop.getType()
+    const yargsType = transformYargsType(mapToYargsType(type))
+    
 
     const jsdoc = getLastJSDoc(prop)
-    if(null === jsdoc) return [ name, { type }]
+    if(null === jsdoc) return [ name, { ...yargsType } ]
     const props = transformJSDoc(jsdoc)
-    return [ name, { ...props, type } ]
+    return [ name, { ...props, ...yargsType } ] as any
   })
 }
 
@@ -84,7 +194,25 @@ function transformJSDocTag(tags: JSDocTag[]): { [key: string]: string } {
     const text = tag.getText()
     const comment = tag.getComment()
     if(undefined === comment) return
-    acc[text.replace(/^@/, ``).trim()] = comment
+    const key: string = text.replace(/^@/, ``).trim()
+    
+    if(YargsControlledTags.includes(key)) {
+      console.log(makeUseControlledTagError(key))
+      return acc
+    } else if(!YargsSupportedTags.includes(key)) {
+      console.log(makeUnUseSupportedTagError(key))
+      return acc
+    }
+    
+    acc[key] = comment
     return acc
   }, Object.create(null))
+}
+
+function makeUseControlledTagError(tag: string) {
+  return new Error(`Option property "${tag}" was controlled, please remove jsdoc tag "@${tag}"`)
+}
+
+function makeUnUseSupportedTagError(tag: string): Error {
+  return new Error(`Option property "${tag}" was not supported, please one of "@${YargsSupportedTags.join(', ')}"`)
 }
--- src/transform-options.test.ts
+++ src/transform-options.test.ts
@@ -1,32 +1,68 @@
-import { Project, ts } from 'ts-morph'
-// import transform, { convertToYargsOption } from './transform-interface-to-yargs-option'
+import { Project, ts, printNode } from 'ts-morph'
 import convert from './transform-option'
 
-test(`transformInterfaceToYargsOption()`, () => {
-  const code: string = `\
-/**
- * @cliOptions
- */
-interface Options {
-  /**
-   * description for Options.foo
-   * @link a
-   * @default 'bar'
-   */
-  foo: string
-  bar: number
-  /** */
-  baz: boolean
-}
+describe(`convert() types`, () => {
+  test(`string`, () => {
+    const code: string = `interface Options { foo: string }`
+    expect(print(code)).toMatchSnapshot()
+  })
+  
+  test(`number`, () => {
+    const code: string = `interface Options { foo: number }`
+    expect(print(code)).toMatchSnapshot()
+  })
+
+  test(`boolean`, () => {
+    const code: string = `interface Options { foo: boolean }`
+    expect(print(code)).toMatchSnapshot()
+  })
+
+  test(`string[]`, () => {
+    const code: string = `interface Options { foo: string[] }`
+    expect(print(code)).toMatchSnapshot()
+  })
+
+  test(`Array<string>`, () => {
+    const code: string = `interface Options { foo: Array<string> }`
+    expect(print(code)).toMatchSnapshot()
+  })
+
+  test(`number[]`, () => {
+    const code: string = `interface Options { foo: number[] }`
+    expect(print(code)).toMatchSnapshot()
+  })
+
+  test(`Array<number>`, () => {
+    const code: string = `interface Options { foo: Array<number> }`
+    expect(print(code)).toMatchSnapshot()
+  })
+
+  test(`boolean[]`, () => {
+    const code: string = `interface Options { foo: boolean[] }`
+    expect(print(code)).toMatchSnapshot()
+  })
+
+  test(`Array<boolean>`, () => {
+    const code: string = `interface Options { foo: Array<boolean> }`
+    expect(print(code)).toMatchSnapshot()
+  })
+
+  test.only(`enum`, () => {
+    const code: string = `\
+enum E { A = 'a', B = 'b' }
+interface Options { foo: E }
 `
+    expect(print(code)).toMatchSnapshot()
+  })
+})
+
+function print(code: string): string {
   const project = new Project({
     skipFileDependencyResolution: true
   })
   const sourceFile = project.createSourceFile(`tmp.ts`, code)
-  const result = convert(sourceFile.getInterfaces()[0])
-  console.log(code, `\n\n`)
-  if(null === result) return
-  const ret = ts.createPrinter().printList(ts.ListFormat.MultiLine, result as any, sourceFile as any)
-  console.log(ret)
-})
-
+  const nodes = convert(sourceFile.getInterfaces()[0])
+  // console.log(require('util').inspect(nodes[0], { depth: null }))
+  console.log(printNode(nodes[0]))
+  return ts.createPrinter().printList(ts.ListFormat.MultiLine, nodes as any, sourceFile as any)
+}
